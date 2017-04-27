8978513f9435880f7202b20c0319a35e77e702d3
nim_13514063 <steffiindrayani16@gmail.com>
2017-04-27 21:28:52 +0700

    update node

--- node.py
+++ node.py
@@ -6,34 +6,39 @@ class nodeInfo:
 	idnode = 0
 	ip = 0
 	port = 0
-	status = "FOLLOWER"
 	
-	def __init__(self, ip, port, status):
-      self.idnode	+= 1
-      self.ip 		= ip
-      self.port 	= port
-      self.status	= status
-      
-    def setStatus(self, status):
-      self.status	= status
-		
+	def __init__(self, ip, port):
+		self.idnode	+= 1
+		self.ip 		= ip
+		self.port 	= port
+
+class rivalC:
+	idnode = 0
+	numvote = 0
 
 class node:
+	nodeInfo = []
+	status = "FOLLOWER"
+	vote = 0
 	timeout = 0
 	startTime = 0
+	timeoutServer = 0
 	listneigh = []
 	listserver = []
+	listrival = []
 
-	def __init__(self):
+	def __init__(self, nInfo):
+		self.nodeInfo	= nInfo;
 		self.timeout	= randint(2,5)
 		self.startTime	= datetime.now()
 	
 	def resetTimeout(self):
 		self.timeout = randint(2,5)
+		self.startTime	= datetime.now()
 		
 	def isTimeOut(self):
-		currNow = datetime.now()
-		if (currNow - startTime >= timeout):
+		now = datetime.now()
+		if ( (now - startTime).total_seconds() >= timeout):
 			 return True
 		return False
 		
@@ -43,4 +48,36 @@ class node:
 	def addServer(self, server):
 		self.listServer.append(server)
 	
+	def candidacyRequest(self):
+		if (self.isTimeOut == True):
+			self.status = "CANDIDATE"
+			'kirim ke semua klo u jd candidate'
+			
+	def recVoteCF(self):
+		'terima reqvote dari candidate ke follower, follower bales response'
+		
+
+	def sendVoteCC(self):
+		'kirim klo lu dapet vote ke candidate lain'
+		
 	
+	def recVoteCC(self):
+		'terima vote dari candidate lain'
+		
+
+	def setLeader(self):
+		'kalo > jadi leader, kalo < jadi follower'
+		
+		
+	def sendHeartbeat(self):
+		'kirim heartbeat(load sm list) ke follower [post request] pake waktu bukan nunggu balesan'
+		
+	
+	def recHeartbeat(self):
+		'terima heartbeat dari follower [langsung response request]'
+		
+		
+	def recServerLoad(self):
+		'terima server load dari daemon'
+
+
--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 
 class server:
-   'identitas satu node'
-   idserver = 0
-   ip 		= 0
-   port 	= 0
-   load 	= 0
+	'identitas satu node'
+	idserver = 0
+	ip 		= 0
+	port 	= 0
+	load 	= 0
+	isDead	= 0
 
+	def __init__(self, ip, port, load):
+		self.idserver =+1
+		self.ip = ip
+		self.port = port
+		self.load = load
 
-   def __init__(self, ip, port, load):
-	   server.idserver =+1
-	   server.ip = ip
-	   server.port = port
-	   server.load = load
-
-	
